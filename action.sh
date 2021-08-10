19389730695429fe91c4ff67d3662250d1950541
RekGRpth <rekgrpth@gmail.com>
2021-08-10 09:28:00 +0500

    commit and push

--- action.sh
+++ action.sh
@@ -2,3 +2,5 @@
 
 set -eux
 sed -Ei "s|^([Cc]opyright.+) ([0-9]{4})([-]?)([0-9]{4}?) (.+)$|\1 \2-$(date "+%Y") \5|g" "${INPUTS_LICENSE:-LICENSE}"
+git commit --message "update license year" --all
+git push --progress
