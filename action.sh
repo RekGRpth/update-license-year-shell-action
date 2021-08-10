9b0fe56eddc48ef23125635a8fafc0a37f35da8a
RekGRpth <rekgrpth@gmail.com>
2021-08-10 09:33:00 +0500

    config

--- action.sh
+++ action.sh
@@ -2,5 +2,7 @@
 
 set -eux
 sed -Ei "s|^([Cc]opyright.+) ([0-9]{4})([-]?)([0-9]{4}?) (.+)$|\1 \2-$(date "+%Y") \5|g" "${INPUTS_LICENSE:-LICENSE}"
+git config --local user.email "${INPUTS_EMAIL:-actions@github.com}"
+git config --local user.name "${INPUTS_NAME:-update license year}"
 git commit --message "update license year" --all
 git push --progress
